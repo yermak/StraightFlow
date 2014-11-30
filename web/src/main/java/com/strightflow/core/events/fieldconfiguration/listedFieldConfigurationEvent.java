46318310f60f57b5468c20d8a458851c2089c8d4
Yaroslav Yermak <yermak@gmail.com>
2014-11-30 14:14:08 +0000

    fix

--- web/src/main/java/com/strightflow/core/events/fieldconfiguration/listedFieldConfigurationEvent.java
+++
@@ -1,9 +0,0 @@
-package com.strightflow.core.events.fieldconfiguration;
-
-import com.strightflow.core.events.LoadedEvent;
-
-/**
- * Created by yermak on 22/5/14.
- */
-public class listedFieldConfigurationEvent extends LoadedEvent {
-}
